8dc5217ed85321444aac8a01fb2efe50a2e59deb
Arpit Panchal <arpitpanchal2914@gmail.com>
2025-02-07 00:06:25 +0530

    add

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -281,7 +281,7 @@ const [gridCols, setGridCols] = useState<number>(2);
                     {/* profile logo  */}
                     <div className=" w-24 p-1 ">
                       <Image
-                        src={`/krishna.jpg`}
+                        src={`./krishna.jpg`}
                         alt={project.title}
                         className="rounded-full w-full  object-cover"
                       />
